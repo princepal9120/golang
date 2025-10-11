4c0d3edc889b5b96d1e8af0021a7b32d346099c6
Prince Pal <princepal9120@gmail.com>
2025-10-11 12:31:16 +0530

    parse config file

--- rest-apis/internal/config/config.go
+++ rest-apis/internal/config/config.go
@@ -1,5 +1,12 @@
 package config
-import "github.com/ilyakaznacheev/cleanenv"
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type HTTPServer struct {
 	Addr string `yaml:"address" env-required:true`
@@ -9,4 +16,32 @@ type Config struct {
 	Env     string `yaml:"env" env:"ENV" env-required:true`
 	Storage string `yaml:"storage_path" env-required:true`
 	HTTPServer `yaml:"http_server" env-required:true`
+}
+
+func MustLoad() *Config {
+
+	var configPath string
+
+	configPath =os.Getenv("CONFIG_PATH")
+	if  configPath=="" {
+		flags :=flag.String("config","", "path to configuration file")
+		flag.Parse()
+
+		configPath=*flags
+		if configPath == "" {
+			log.Fatal("path is not set") 
+		}
+
+	}
+	if _ , err :=os.Stat(configPath);os.IsNotExist(err) {
+		log.Fatalf("config file does not exits: %s", configPath)
+	} 
+
+	var cfg Config
+	err :=cleanenv.ReadConfig(configPath,&cfg)
+	if err !=nil {
+		log.Fatalf("can not read config file: %s", err.Error())
+	}
+	return &cfg;
+
 }
