8710b66f7d928b6f26e4415905e7db142b51e611
Prince Pal <princepal9120@gmail.com>
2025-10-31 23:13:19 +0530

    comlete go server in restapis

--- rest-apis/cmd/rest-apis/main.go
+++ rest-apis/cmd/rest-apis/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/princepal9120/rest-apis/internal/config"
 )
@@ -25,11 +31,29 @@ func main() {
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
-	fmt.Println("server started")
-	err := server.ListenAndServe()
+	fmt.Printf("server started %s\n", cfg.HTTPServer.Addr)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	if err != nil {
-		log.Fatalf("server not started: %s", err.Error())
+	// Wait for interrupt signal to gracefully shutdown the server
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatalf("server not started: %s", err.Error())
+		}
+	}()
+	<-done
+
+
+	slog.Info("server stopped")
+
+
+	ctx,cancel:=context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+	err:=server.Shutdown(ctx)
+	if err!=nil {
+		slog.Error("server shutdown failed:", slog.String("error", err.Error()))
 	}
-
+	slog.Info("server exited properly")
 }
